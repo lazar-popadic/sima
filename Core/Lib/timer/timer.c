9a286d6a76770b5178041d69f47c1760ce85ac23
lazuskica <lazuskica@gmail.com>
2023-11-30 13:05:35 +0100

    Initial commit

---
+++ Core/Lib/timer/timer.c
@@ -0,0 +1,13 @@
+/*
+ * timer.c
+ *
+ *  Created on: Nov 30, 2023
+ *      Author: lazar
+ */
+
+#include "timer.h"
+
+#define START_TIME 90*1000
+#define END_TIME 100*1000
+
+
---
+++ Core/Lib/timer/timer.h
@@ -0,0 +1,13 @@
+/*
+ * timer.h
+ *
+ *  Created on: Nov 30, 2023
+ *      Author: lazar
+ */
+
+#ifndef LIB_TIMER_TIMER_H_
+#define LIB_TIMER_TIMER_H_
+
+
+
+#endif /* LIB_TIMER_TIMER_H_ */
